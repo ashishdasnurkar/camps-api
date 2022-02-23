75dc14a73d53a79f7c14bc756dfe37e4537659ba
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-02-23 23:15:14 +1100

    added another module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { CampModule } from './camp/camp.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, CampModule],
 })
 export class AppModule {}
