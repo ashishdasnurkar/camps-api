0446ebf7f572efbf9371d6dbdd9c9dba59cae9de
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-02-25 22:54:21 +1100

    use auth service from controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
   @Post('signup')
   signup() {
-    return "I'm signed up...";
+    return this.authService.signup();
   }
 
   @Post('signin')
   signin() {
-    return "I'm signed in...";
+    return this.authService.signin();
   }
 }
