796d4326598d512f16b50abb25fc7b49ac207108
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-02-28 20:38:15 +1100

    created prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CampModule } from './camp/camp.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, CampModule],
+  imports: [AuthModule, UserModule, CampModule, PrismaModule],
 })
 export class AppModule {}
