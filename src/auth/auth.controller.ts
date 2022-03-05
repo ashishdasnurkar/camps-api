4b7df26550d7a82ae29c2a9d50a9f896156e27e4
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-03-05 23:25:23 +1100

    use dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup(@Req() req: Request) {
-    console.log(req.body);
+  signup(@Body() dto: any) {
+    console.log({
+      dto: dto,
+    });
     return this.authService.signup();
   }
 
