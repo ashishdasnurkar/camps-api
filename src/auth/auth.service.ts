501bdf9048deec87e77b52e0810dd3b20138b0bf
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-03-18 10:20:15 +1100

    implement signin endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -29,7 +29,26 @@ export class AuthService {
       }
     }
   }
-  signin() {
-    return { msg: 'I have signed in!' };
+  async signin(dto: AuthDto) {
+    // find the user with email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // if no user found throw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials not found!');
+    }
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if password doesn't match then throw error
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials not found!');
+    }
+    // send the user back
+    delete user.hash;
+    return user;
   }
 }
