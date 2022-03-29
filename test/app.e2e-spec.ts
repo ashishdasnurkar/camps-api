498b62e451d959a533d91cb6b69857f86bd57eaf
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-03-29 21:29:33 +1100

    setup test app and teardown

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,24 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 describe('App e2e', () => {
+  let app: INestApplication;
   beforeAll(async () => {
-    await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+    app = await moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
+  });
+
+  afterAll(() => {
+    app.close();
   });
   it.todo('should pass');
 });
