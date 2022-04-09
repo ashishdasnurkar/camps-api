87af4c7362a9d4acd541494655b830a2081520ce
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-04-09 23:51:35 +1000

    get user test with valid user AT from previous signin

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -97,13 +97,24 @@ describe('App e2e', () => {
           .spec()
           .post('/auth/signin')
           .withBody(dto)
-          .expectStatus(200);
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
       });
     });
   });
 
   describe('User', () => {
-    describe('Get Me', () => {});
+    describe('Get Me', () => {
+      it('Should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200);
+      });
+    });
   });
 
   describe('Camp', () => {
