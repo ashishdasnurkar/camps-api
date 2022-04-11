f682b31904b6efdbd177b451e18b1ae10574d475
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-04-11 20:14:59 +1000

    add edit user controller and service metods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
-import { Request } from 'express';
 import { GetUser } from '../auth/decorator';
 import { User } from '@prisma/client';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @UseGuards(JwtGuard)
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
